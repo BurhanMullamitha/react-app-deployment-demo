0121d53fa4c1db5732736f7fa04d14602ebde1b2
BurhanMullamitha <BurhanMullamitha@users.noreply.github.com>
2020-06-25 18:44:35 +0000

    deploy: 0a2d872f46d76171fef7ac44a6968231236d1d40

---
+++ precache-manifest.7c0159a9523294e14e181a56c78a6429.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496005fdfe0cbbb518dd2b3989e7f9e2",
+    "url": "/react-app-deployment-demo/index.html"
+  },
+  {
+    "revision": "300225c7f6c68f7f7daa",
+    "url": "/react-app-deployment-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "51f8f1b0b823a7348c22",
+    "url": "/react-app-deployment-demo/static/js/2.74a57033.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-deployment-demo/static/js/2.74a57033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "300225c7f6c68f7f7daa",
+    "url": "/react-app-deployment-demo/static/js/main.0749cb74.chunk.js"
+  },
+  {
+    "revision": "b1d42f8dc7d75cbed4a0",
+    "url": "/react-app-deployment-demo/static/js/runtime-main.7d3b5d92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-deployment-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
